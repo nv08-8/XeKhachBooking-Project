366931b6ca330410f1456f0bcb17d5b71e6fe99c
nv08-8 <nhuvonguyen2005@gmail.com>
2025-11-15 06:10:37 +0700

    .

--- backend_api/utils/sendEmail.js
+++ backend_api/utils/sendEmail.js
@@ -1,20 +1,19 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, text) {
-    const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS,
-        },
-    });
+    try {
+        const data = await resend.emails.send({
+            from: "GoUTE <onboarding@resend.dev>",
+            to: to,
+            subject: subject,
+            text: text,
+        });
 
-    await transporter.sendMail({
-        from: `"GoUTE" <${process.env.MAIL_USER}>`,
-        to,
-        subject,
-        text,
-    });
+        console.log("📧 Email sent:", data);
+    } catch (error) {
+        console.error("❌ Send email failed:", error);
+    }
 }
 
 module.exports = sendEmail;
