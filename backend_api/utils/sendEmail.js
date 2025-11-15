612c6b55d7bda8f52b3be6b0ffb373b9be816a8c
nv08-8 <nhuvonguyen2005@gmail.com>
2025-11-15 10:38:51 +0700

    Update sendEmail.js

--- backend_api/utils/sendEmail.js
+++ backend_api/utils/sendEmail.js
@@ -1,21 +1,19 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-async function sendEmail(to, subject, text) {
+async function sendEmail(to, otp) {
     try {
         const msg = {
             to,
-            from: {
-                email: process.env.SENDGRID_FROM,
-                name: "GoUTE Ticket System"  // thêm tên sẽ tăng uy tín email
-            },
-            subject,
-            text,
-            html: `<p>${text}</p>`,     // có HTML sẽ ít bị đánh spam hơn
+            from: process.env.SENDGRID_FROM,
+            templateId: process.env.SENDGRID_TEMPLATE_ID,
+            dynamic_template_data: {
+                otp
+            }
         };
 
-        const response = await sgMail.send(msg);
-        console.log("📧 Email sent!", response[0].statusCode);
+        await sgMail.send(msg);
+        console.log("📧 OTP sent!");
     } catch (error) {
         console.error("❌ SendGrid Error:", error.response?.body || error);
     }
